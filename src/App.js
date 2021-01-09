5995ae9eff2a250c5d73899e9e13c75f59da8e2e
alligatormonday <joey@joeyjepson.com>
2021-01-08 20:35:36 -0700

    imported react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
+import { Switch, Route, Router} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <>
-
-    
-
+      <Router>
 
 
+        {/* Nav Bar */}
+
+
+        {/* Parent div */}
+        <Switch>
+          <Route exact path="/, /contact"/>
+        </Switch>
+
+
+
+      </Router>
     </>
   );
 }
